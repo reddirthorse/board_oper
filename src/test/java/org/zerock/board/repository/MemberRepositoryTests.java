03d1251b2eb2b030d6b491abeb84f99dc9087e3e
reddirthorse <operstu1@gmail.com>
2021-11-10 16:31:37 +0900

    Initial commit

---
+++ src/test/java/org/zerock/board/repository/MemberRepositoryTests.java
@@ -0,0 +1,27 @@
+package org.zerock.board.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.board.entity.Member;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void insertMemners(){
+        IntStream.rangeClosed(1,100).forEach(i ->{
+            Member member = Member.builder()
+                    .email("user"+i+"@aaa.com")
+                    .passward("2222")
+                    .name("USER"+i)
+                    .build();
+            memberRepository.save(member);
+        });
+    }
+}
